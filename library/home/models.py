8f97831f3bdaefd570f2012436cbccc57bc83bc7
Talha Malik <talha.malik@A006-01262.local>
2023-08-09 18:04:54 +0500

    Created book model class

--- library/home/models.py
+++ library/home/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    """
+    User Class inherited from in-built AbstractUser 
+    Class from django.contrib.auth.models
+    """
     full_name = models.CharField(max_length=255)
     phone = models.CharField(max_length=20, blank=True, null=True)
     gender_choices = (
@@ -29,3 +33,13 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+class Book(models.Model):
+    # name, image, author name, publisher and number of books
+    name = models.CharField(max_length=50)
+    author_name = models.CharField(max_length=50)
+    publisher_name = models.CharField(max_length=50)
+    number_of_books = models.PositiveIntegerField()
+    #To-Do : Add the image field  
+
+    def __str__(self):
+        return self.name
--- library/home/urls.py
+++ library/home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import homePage
 
 urlpatterns = [
-    path('', homePage, name="home"),
+    path('', homePage, name='home'),
 ]

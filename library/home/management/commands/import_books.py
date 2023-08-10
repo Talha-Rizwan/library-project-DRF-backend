108425e44dd77a7a441c6b981781a2e916b6fb92
Talha Malik <talha.malik@A006-01262.local>
2023-08-10 11:37:32 +0500

    Feat: custom command to create books from csv file

---
+++ library/home/management/commands/import_books.py
@@ -0,0 +1,27 @@
+'''customize managment commands classes'''
+import csv
+from django.core.management.base import BaseCommand
+from home.models import Book
+
+# Command to run : python manage.py import_books /Users/talha.malik/Desktop/library_project/library_management_system/books_data.csv
+
+class Command(BaseCommand):
+    '''customize command to create new books from csv file'''
+    help = "import new books from the csv file"
+
+    def add_arguments(self, parser):
+        '''method to read and add command line arguments'''
+        parser.add_argument('csv_file', type=str, help='Path to the CSV file')
+
+    def handle(self, *args, **kwargs):
+        '''method to create new users by reading data from csv file'''
+        csv_file = kwargs['csv_file']
+
+        with open(csv_file,encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                book = Book.objects.create(name=row[0], author_name=row[1], publisher_name=row[2], number_of_books=int(row[3]))
+
+                self.stdout.write(self.style.SUCCESS(f'Successfully imported book: {book}'))
--- library/home/urls.py
+++ library/home/urls.py
@@ -3,5 +3,5 @@ from .views import homePage, post_book
 
 urlpatterns = [
     path('', homePage, name='home'),
-    path('book',post_book, name='book' )
+    path('post-book',post_book, name='post-book' )
 ]

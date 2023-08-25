ee025393f99870387376a3114bdc5c09f6b6c2e3
Talha Malik <talha.malik@A006-01262.local>
2023-08-25 16:48:24 +0500

    added more unit tests for BookViewSet

--- library/home/tests/test_books.py
+++ library/home/tests/test_books.py
@@ -5,23 +5,23 @@ from rest_framework.test import APITestCase
 
 from home.serializers import BookSerializer
 from home.tests.factories import BookFactory
-from userapp.models import User
 from home.models import Book
 from userapp.tests.factories import UserFactory
 from userapp.utlis import get_jwt_token
 
 class BookViewSetTestCase(APITestCase):
+
     def setUp(self):
         self.books = BookFactory.create_batch(10)
-        self.test_user = UserFactory()
+        self.customer_user = UserFactory()
+        self.librarian_user = UserFactory()
         self.url = '/api/home/book-view-set/'
-
         librarian = Permission.objects.get(codename='is_librarian')
-        self.test_user.user_permissions.add(librarian)
-        self.test_user.save()
+        self.librarian_user.user_permissions.add(librarian)
+        self.librarian_user.save()
   
         data = {
-            "username": self.test_user.username,
+            "username": self.librarian_user.username,
             "password": 'password123'
         }
         token = get_jwt_token(data)['data']['token']['access']
@@ -44,10 +44,39 @@ class BookViewSetTestCase(APITestCase):
         self.assertEqual(response.data['name'], data['name'])
 
 
+    def test_create_book_without_credentials(self):
+        self.client.credentials()
+        serializer = BookSerializer(BookFactory.build())
+        data = serializer.data
+        del data['cover_image']
+        response = self.client.post(self.url, data=data, format='json')        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_create_book_with_customer_user(self):
+        user = {
+            "username": self.customer_user.username,
+            "password": 'password123'
+        }
+        token = get_jwt_token(user)['data']['token']['access']
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+        serializer = BookSerializer(BookFactory.build())
+        data = serializer.data
+        del data['cover_image']
+        response = self.client.post(self.url, data=data, format='json')        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
     def test_retrieve_book(self):
         self.client.credentials()
         response = self.client.get(f'{self.url}{self.books[0].id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_retrieve_book_with_invalid_id(self):
+        self.client.credentials()
+        response = self.client.get(f'{self.url}100/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
     def test_update_book(self):
@@ -62,10 +91,32 @@ class BookViewSetTestCase(APITestCase):
         self.assertEqual(response.data['number_of_books'], 10)
 
 
+    def test_update_book_with_invalid_id(self):
+        serializer = BookSerializer(self.books[0])
+        data = serializer.data
+        data['name'] = "Updated Book Name"
+        data["number_of_books"] = 10
+        del data['cover_image']
+        response = self.client.put(f'{self.url}100/', data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
     def test_delete_book(self):
         response = self.client.delete(f'{self.url}{self.books[0].id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Book.objects.filter(id=1).exists())
+
+
+    def test_delete_book_with_unauthorized_user(self):
+        user = {
+            "username": self.customer_user.username,
+            "password": 'password123'
+        }
+        token = get_jwt_token(user)['data']['token']['access']
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+
+        response = self.client.delete(f'{self.url}{self.books[0].id}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
     def test_search_books(self): 
@@ -73,3 +124,10 @@ class BookViewSetTestCase(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(self.books[0].id, response.data[0]['id'])
+
+
+    def test_search_books_with_wrong_param(self): 
+        url = f'{self.url}?wrong={self.books[0].name}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['data']), 10)
